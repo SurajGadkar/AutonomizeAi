2c909e8049e08fb856ec11723b5a273bdf9bef25
Suraj Gadkar <surajgadkar14@gmail.com>
2024-01-24 16:34:19 +0530

    Netlify config

--- client/autonomizeClient/vite.config.js
+++ client/autonomizeClient/vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  optimizeDeps: {
+    include: [
+      "react",
+      "react-dom",
+      "react-router-dom",
+      // Add any other dependencies that should be treated as external
+    ],
+  },
+});
